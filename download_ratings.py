af7a535111e09c8f56a3e41f50839a37d0837c23
Matt Haechrel <haechrelmatt@gmail.com>
2023-12-02 22:11:13 -0600

    Ensure browser object gets closed, even if code is interrupted due to errors.

--- download_ratings.py
+++ download_ratings.py
@@ -1,11 +1,25 @@
 import argparse
 import os
 import pathlib
+import sys
+import traceback
 
 from playwright.sync_api import sync_playwright
 from playwright._impl._errors import Error as PlaywrightAPIError
 
 from read_config import read_config
+
+
+def cleanup(web_browser, web_page=None):
+    if web_page is not None:
+        web_page.close()
+    web_browser.close()
+    return None
+
+
+def raise_error():
+    # Force an error. Here for testing purposes.
+    raise RuntimeError('A fake error occurred.')
 
 
 if __name__ == '__main__':
@@ -20,19 +34,27 @@ if __name__ == '__main__':
     this_dir = pathlib.Path(__file__).parent.resolve()
 
     with sync_playwright() as p:
+        page = None
         chromium = p.chromium
         browser = chromium.launch_persistent_context(user_data_dir, headless=True)
-        api_request_context = browser.request
-        page = browser.new_page()
-        page.goto(imdb_ratings_page)
-        url = page.url
-        imdb_user_nbr = url.split('https://www.imdb.com/user/ur', 1)[1][:8]
-        imdb_ratings_download = f"https://www.imdb.com/user/ur{imdb_user_nbr}/ratings/export"
-        with page.expect_download() as download_info:
-            try:
-                page.goto(imdb_ratings_download)
-            except PlaywrightAPIError:
-                pass
-        download = download_info.value
-        download.save_as(os.path.join(this_dir, 'ratings.csv'))
-        browser.close()
+        try:
+            exit_code = 0
+            api_request_context = browser.request
+            page = browser.new_page()
+            page.goto(imdb_ratings_page)
+            url = page.url
+            imdb_user_nbr = url.split('https://www.imdb.com/user/ur', 1)[1][:8]
+            imdb_ratings_download = f"https://www.imdb.com/user/ur{imdb_user_nbr}/ratings/export"
+            with page.expect_download() as download_info:
+                try:
+                    page.goto(imdb_ratings_download)
+                except PlaywrightAPIError:
+                    pass
+            download = download_info.value
+            download.save_as(os.path.join(this_dir, 'ratings.csv'))
+        except Exception:
+            traceback.print_exc()
+            exit_code = 1
+        finally:
+            cleanup(browser, page)
+            sys.exit(exit_code)
